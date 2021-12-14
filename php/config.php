ab89e82b95ff40780b9be56abc920b1c7c0ee29d
kadjiamal <kadjiamalo@gmail.com>
2021-12-14 21:16:09 +0700

    change to cleardb

--- php/config.php
+++ php/config.php
@@ -1,12 +1,11 @@
-<?php 
- 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "yukbisnis";
- 
-$conn = mysqli_connect($server, $user, $pass, $database);
- 
-if (!$conn) {
-    die("<script>alert('Gagal tersambung dengan database.')</script>");
-}
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
